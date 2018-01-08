787c75d8533edd3a3189e4bcb94cab4ca19e9348
Matt Trussell <smtrussell@gmail.com>
2018-01-08 06:48:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -80,7 +80,11 @@ class Dog
   end
 
   def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
 
+    dog = Dog.new(id: id, name: name, breed: breed)
   end
 
   def self.find_by_name(search_name)
